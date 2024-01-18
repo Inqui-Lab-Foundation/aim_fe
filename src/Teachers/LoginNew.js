af839fdf6dea907de76fbcd732fe912eb4fce4c8
ramant <ramant@inqui-lab.org>
2024-01-18 12:31:18 +0530

    header

--- src/Teachers/LoginNew.js
+++ src/Teachers/LoginNew.js
@@ -233,7 +233,7 @@ const LoginNew = (props) => {
                                                 className="mb-2"
                                                 htmlFor="phone"
                                             >
-                                                Email Address2
+                                                Email Address
                                             </Label>
                                             <InputBox
                                                 {...inputUserId}
